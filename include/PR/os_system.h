15302e8a74487bd35ad39e975fa75648f45a0e4a
CrashOveride95 <crashoveride953@gmail.com>
2023-06-28 12:56:00 -0400

    fix void in os_system.h

--- include/PR/os_system.h
+++ include/PR/os_system.h
@@ -102,7 +102,7 @@ extern OSIntMask __OSGlobalIntMask;	/* global interrupt mask */
  */
 
 extern void		osInitialize(void);
-extern void             osInitialize_isv();
+extern void             osInitialize_isv(void);
 extern void		osExit(void);
 extern u32 		osGetMemSize(void);
 
